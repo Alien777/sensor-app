ab2ed7789252bd1e437c01175a99dccba1c9423c
adam <lasota.adam.92@gmail.com>
2024-07-06 20:42:46 +0200

    Added ping and timer for pwm

--- firmwares/ESP-32S/src/include/sensor_runner.h
+++ firmwares/ESP-32S/src/include/sensor_runner.h
@@ -4,6 +4,7 @@
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include "freertos/timers.h"
 #include <driver/adc.h> //DEPRICATED
 #include "sensor_mqtt.h"
 #include "sensor_structure.h"
